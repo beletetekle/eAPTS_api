4123a95b4dbe372e348c671b5033fc41a1367c9d
Yididya <yididiya.yilma@gmail.com>
2018-08-04 12:15:10 +0300

    Rename HealthCenter => HealthFacitlity

--- server/boot/roles.js
+++ server/boot/roles.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     var Region = app.models.Region;
     var Zone = app.models.Zone;
     var Woreda = app.models.Woreda;
-    var HealthCenter = app.models.HealthCenter;
+    var HealthFacility = app.models.HealthFacility;
 
 
     
